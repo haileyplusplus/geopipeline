b9ea4a6b55ca21af11ab53c2bd345bd6017a42f4
hailey <mceuen4@gmail.com>
2024-03-11 20:50:39 -0500

    Refactor

--- opendatafetcher.py
+++ opendatafetcher.py
@@ -4,6 +4,8 @@ import argparse
 import datetime
 import os
 import subprocess
+import tempfile
+import zipfile
 
 from peewee import SqliteDatabase, Model, CharField, DateTimeField, BooleanField
 import sanitize_filename
@@ -11,14 +13,16 @@ import sanitize_filename
 from opendatabrowser import Browser
 
 """
-Next: extract automatically and give better names
+Next: 
+ extract automatically and give better names
+ explore arcgis db tables in sqlite
 """
 
 DESTINATION_DIR = '/Users/hailey/Documents/ArcGIS/data/chicago'
 db = SqliteDatabase(os.path.join(DESTINATION_DIR, 'fetchermetadata.sqlite3'))
 
 
-class DataSource(Model):
+class DataSet(Model):
     id_ = CharField()
     name = CharField()
     retrieved = DateTimeField()
@@ -29,14 +33,15 @@ class DataSource(Model):
         database = db
 
 
-class Fetcher:
+class DataSetInstance:
     URL_TEMPLATE = 'https://data.cityofchicago.org/api/geospatial/%s?method=export&format=Shapefile'
 
-    def __init__(self, args):
-        self.browser = Browser(None)
+    def __init__(self, key):
+        self.key = key
 
-    def fetch(self, key):
-        item = self.browser.get(key)
+    def fetch(self, browser):
+        key = self.key
+        item = browser.get(key)
         if not item:
             print(f'Error fetching {key}')
             return False
@@ -46,11 +51,11 @@ class Fetcher:
         if type_ != 'map':
             print(f'  Error attempting to fetch non-map')
             return False
-        self.inner_fetch(key, name)
+        self.inner_fetch(name)
         return True
 
-    def inner_fetch(self, key, name):
-        url = self.URL_TEMPLATE % key
+    def inner_fetch(self, name):
+        url = self.URL_TEMPLATE % self.key
         fetch_time = datetime.datetime.now()
         filename = sanitize_filename.sanitize(f'{name}.zip')
         fullpath = os.path.join(DESTINATION_DIR, filename)
@@ -60,8 +65,26 @@ class Fetcher:
             return
         cp = subprocess.run(['curl', '-o', fullpath, url])
         success = cp.returncode == 0
-        src = DataSource(id_=key, name=name, retrieved=fetch_time, fullpath=fullpath, success=success)
+        src = DataSet(id_=self.key, name=name, retrieved=fetch_time, fullpath=fullpath, success=success)
         src.save()
+
+    def extract(self):
+        pass
+
+
+class Fetcher:
+    def __init__(self):
+        self.browser = Browser(None)
+        self.datasets = []
+
+    def add(self, key):
+        self.datasets.append(DataSetInstance(key))
+
+    def process_all(self, force):
+        for d in self.datasets:
+            fetched = d.fetch(self.browser)
+            if not fetched and force:
+                d.inner_fetch(k)
 
 
 if __name__ == "__main__":
@@ -73,9 +96,8 @@ if __name__ == "__main__":
     parser.add_argument('--force', action='store_true')
     args = parser.parse_args()
     db.connect()
-    db.create_tables([DataSource])
-    f = Fetcher(args)
+    db.create_tables([DataSet])
+    f = Fetcher()
     for k in args.key:
-        fetched = f.fetch(k)
-        if not fetched and args.force:
-            f.inner_fetch(k, k)
+        f.add(k)
+    f.process_all(force=args.force)
