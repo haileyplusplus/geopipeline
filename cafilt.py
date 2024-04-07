5840c0039f1d1b4a17d58b6bc1fbc1b1fae9d95b
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-04-06 23:56:33 -0500

    Switch filter to use community areas instead of neighborhoods and to allow multiple to be specified.

--- cafilt.py
+++ cafilt.py
@@ -2,32 +2,34 @@
 
 import os
 import sys
+from typing import List
 
 import geopandas as gpd
 
-COMMUNITY_AREAS = '/Users/hailey/tmp/mapcache/Boundaries - Neighborhoods.geojson'
+COMMUNITY_AREAS = '/Users/hailey/tmp/mapcache/Boundaries - Community Areas (current).geojson'
 
 
-def filter(gdf: gpd.GeoDataFrame, community_area: str) -> gpd.GeoDataFrame:
+def filter(gdf: gpd.GeoDataFrame, community_areas: List[str]) -> gpd.GeoDataFrame:
     neighborhoods = gpd.read_file(COMMUNITY_AREAS)
     # lower case
-    n = neighborhoods[neighborhoods.pri_neigh == community_area]
+    n = neighborhoods[neighborhoods.community.isin(community_areas)]
     if n.empty:
-        print(f'Neighborhood (community area) {community_area} not found')
+        print(f'Neighborhood (community area) {community_areas} not found')
         return gpd.GeoDataFrame()
     return gdf.clip(n.envelope)
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
-    neighborhood = sys.argv[2]
+    neighborhoods = sys.argv[2:]
     path, ext = os.path.splitext(filename)
     gdf = gpd.read_file(filename)
-    filtered = filter(gdf, neighborhood)
+    filtered = filter(gdf, neighborhoods)
     if filtered.empty:
         sys.exit(1)
     print(filtered)
-    output = f'{path}.{neighborhood}{ext}'
+    nstr = '.'.join(neighborhoods)
+    output = f'{path}.{nstr}{ext}'
     print(f'Filtered to {output}')
     filtered.to_file(output, driver='GeoJSON')
 
