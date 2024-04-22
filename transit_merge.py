02bded8b6df3401c300a0faf15d184a2dee42b8c
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-04-22 13:41:37 -0500

    Join GTFS data with routes shapefile.

--- transit_merge.py
+++ transit_merge.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import os
+
 import requests
 from gtfs_functions import Feed
 
 from pipeline_interface import PipelineInterface, PipelineResult
+
+DESTINATION_DIR = '/Users/hailey/Documents/ArcGIS/data/chicago'
 
 
 class TransitMerge(PipelineInterface):
@@ -11,6 +15,12 @@ class TransitMerge(PipelineInterface):
         super().__init__(*args, **kwargs)
 
     def run_stage(self) -> PipelineResult:
-        rv = PipelineResult.mark_incomplete()
+        rv = PipelineResult()
+        feed = self.get_dependency('feed_load').get()
+        routes = self.get_dependency('routes_fetch').get()
+        feed2 = feed.lines_freq.drop(columns=['geometry'])
+        m = routes.merge(feed2, left_on='ROUTE', right_on='route_id')
+        rv.obj = m[m.window == '10:00-16:00'].groupby('route_name').apply(lambda x: x.loc[x.ntrips.idxmax()].drop(['route_name']))
+        rv.obj.crs = 3435
+        rv.obj.to_file(os.path.join(DESTINATION_DIR, 'bus_route_frequency.shp'))
         return rv
-
