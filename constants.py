eb3275c96e589d54b884a09c9920c4a55965ab19
hailey <188331+haileyplusplus@users.noreply.github.com>
2024-06-06 16:51:46 -0500

    Use TOML for path configuration.

--- constants.py
+++ constants.py
@@ -1,17 +1,29 @@
-import os
+import tomllib
 
 from pathlib import Path
 
 CHICAGO_DATUM = 26916
 
 
+class ConfigReader:
+    def __init__(self):
+        with open('config.toml', 'rb') as f:
+            config = tomllib.load(f)
+            self.datasets = config['datasets_path']
+            self.shapefile = config['shapefile_path']
+            self.pipeline_cache = config['pipeline_cache_path']
+
+
+configreader = ConfigReader()
+
+
 def datasets_path():
-    return Path(os.getenv('DATASETS_DIR'))
+    return Path(configreader.datasets).expanduser()
 
 
 def shapefile_path():
-    return Path('/Users/hailey/Documents/ArcGIS/data/chicago')
+    return Path(configreader.shapefile).expanduser()
 
 
 def pipeline_cache_path():
-    return Path('/Users/hailey/tmp/pipelinecache')
+    return Path(configreader.pipeline_cache).expanduser()
